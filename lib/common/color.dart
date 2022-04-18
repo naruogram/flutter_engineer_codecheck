924b14296d5caf2e69cf93c4dc0cd1d444d23aa6
Yoshiki Naruo <75286616+yoshiki20010524@users.noreply.github.com>
2022-04-19 04:29:53 +0900

    多言語対応
    l10nを使い、多言語対応

--- lib/common/color.dart
+++ lib/common/color.dart
@@ -1,4 +1,4 @@
 import 'package:flutter/material.dart';
 
-const kPrimaryColorDark = Color(0xFF212121);
 const kPrimaryColor = Color(0xFFF8F8F8);
+const kPrimaryColorDark = Color(0xFF212121);
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_engineer_codecheck/l10n/l10n.dart';
 import 'package:flutter_engineer_codecheck/presentaion/pages/search/search_detail_page.dart';
 import 'package:flutter_engineer_codecheck/presentaion/pages/search/search_page.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() async {
@@ -14,6 +16,17 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      localizationsDelegates: L10n.localizationsDelegates,
+      supportedLocales: L10n.supportedLocales,
+      localeResolutionCallback: (locale, supportedLocales) {
+        if (locale != null) {
+          final _locale = Locale(locale.languageCode);
+          if (supportedLocales.contains(_locale)) {
+            return _locale;
+          }
+        }
+        return supportedLocales.first;
+      },
       home: const SearchPage(),
       theme: ThemeData.light(),
       darkTheme: ThemeData.dark(),
